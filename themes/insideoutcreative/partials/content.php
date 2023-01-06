8fe7c25cc76dff0a8f437edc7d3d58e45a25b605
Efrain Lemus <efraintlemusm@gmail.com>
2023-01-06 11:07:19 -0700

    add repeated section

--- themes/insideoutcreative/partials/content.php
+++ themes/insideoutcreative/partials/content.php
@@ -34,7 +34,7 @@ if($layout == 'Content + Image'){
         echo '</section>';
     endwhile; endif;
 
-} if($layout == 'Text Columns'){
+} elseif($layout == 'Text Columns'){
     if(have_rows('text_columns')): while(have_rows('text_columns')): the_row();
     $bgImg = get_sub_field('background_image');
     $style = get_sub_field('style');
@@ -71,7 +71,129 @@ if($layout == 'Content + Image'){
     echo '</section>';
 
     endwhile; endif;
-} if($layout == 'Skewed Columns'){
+} elseif ($layout == 'Repeated Content') {
+
+    if(have_rows('repeated_content')): while(have_rows('repeated_content')): the_row();
+
+    if(have_rows('sections')):
+    echo '<section class="pt-5 content-area" style="background:url(//cdn.shopify.com/s/files/1/0280/8004/6214/t/5/assets/image-nav-background.jpg?v=99484492238036325261659978185);">';
+        echo '<div class="container-fluid">';
+
+        while(have_rows('sections')): the_row();
+
+        $imageSide = get_sub_field('image_side');
+
+        if($imageSide == 'Left'){
+            echo '<div class="row">';
+        } else {
+            echo '<div class="row flex-row-reverse">';
+            // echo '</div>';
+        }
+        echo '<div class="col-md-6 p-0">';
+        $image = get_sub_field('image');
+        echo wp_get_attachment_image($image['id'],'full','',['class'=>'w-100 h-100','style'=>'object-fit:cover;']);
+        // echo '<img src="//cdn.shopify.com/s/files/1/0280/8004/6214/t/5/assets/Photos-Custom-Gift-Boxes.jpg?v=175517787959240699551659978186" alt="" width="100%" height="100%" style="object-fit:cover;">';
+        echo '</div>';
+
+        if($imageSide == 'Left'){
+        echo '<div class="col-md-6 p-0 right">';
+        } else {
+            echo '<div class="col-md-6 p-0 left">';
+        }
+        echo '<div class="col-md-12 pt-5 pb-5">';
+        echo '<div class="top">';
+
+
+        if($imageSide == 'Left'){
+        echo '<div class="bg-top" style="clip-path: polygon(10% 0%, 100% 0, 100% 100%, 0% 100%);
+        "></div>';
+        } else {
+        echo '<div class="bg-top" style="clip-path: polygon(0% 0%, 90% 0%, 100% 100%, 0% 100%);
+        "></div>';
+        }
+
+        echo '<div class="text-center position-relative z-1">';
+        echo '<div class="">';
+        echo '<h3 class="text-accent-quinary bold text-uppercase h6" style="transform:translate(-75px, 10px);letter-spacing:0.2em;">' . get_sub_field('pretitle') . '</h3>';
+        echo '</div>';
+        echo '<div class="">';
+        echo '<h2 class="text-white" style="font-size:60px;">' . get_sub_field('title') . '</h2>';
+        echo '</div>';
+        // echo '<div class="text-right">';
+        // echo '<h5 class="accent text-uppercase ls-big nova-bold">With our high-quality materials</h5>';
+        // echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="pt-3 pb-5"></div>';
+
+        if($imageSide == 'Left'){
+        echo '<div class="col-md-11 ml-auto mt-2 smaller-text">';
+        } else {
+            echo '<div class="col-md-11 mt-2 smaller-text">';
+            // echo '</div>';
+        }
+        echo '<div class="bg-top"></div>';
+        echo '<div class="featured-text">';
+        echo get_sub_field('content');
+        // echo '<p class="m-0 nova-thin">Named for ski resorts in the Rocky Mountains, our men’s watches and bracelets feature the same rustic charm with rugged, yet elegant, style. Our sustainably sourced materials mean you can wear nature on your wrist while feeling good about being a steward of the environment.</p>';
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="col-md-12 text-center pt-4 pb-4">';
+        $link = get_sub_field('link');
+if( $link ): 
+$link_url = $link['url'];
+$link_title = $link['title'];
+$link_target = $link['target'] ? $link['target'] : '_self';
+echo '<a class="btn-outline" href="' . esc_url( $link_url ) . '" target="' . esc_attr( $link_target ) . '">' . esc_html( $link_title ) . '</a>';
+// echo '<a href="/pages/watches" class="btn primary mb-5">Shop Now</a>';
+endif;
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        endwhile;
+
+        // echo '<div class="row">';
+        // echo '<div class="col-md-6 p-0 left">';
+        // echo '<div class="col-md-12 pt-5 pb-5">';
+        // echo '<div class="top">';
+        // echo '<div class="bg-top"></div>';
+        // echo '<div class="text">';
+        // echo '<div class="text-left">';
+        // echo '<h5 class="accent text-uppercase nova-bold">From Around the World</h5>';
+        // echo '</div>';
+        // echo '<div class="text-center">';
+        // echo '<h3 class="title nova-thin ls-0">exotic wood</h3>';
+        // echo '</div>';
+        // echo '<div class="text-right">';
+        // echo '<h5 class="accent text-uppercase ls-big nova-bold">durable and long lasting</h5>';
+        // echo '</div>';
+        // echo '</div>';
+        // echo '</div>';
+        // echo '</div>';
+        // echo '<div class="pt-3 pb-5"></div>';
+        // echo '<div class="col-md-11 mt-2 smaller-text text-right">';
+        // echo '<div class="bg-top"></div>';
+        // echo '<div class="featured-text">';
+        // echo '<p class="m-0 nova-thin">Premium exotic raw woods. From dark, luxury woods to pacific coast Redwoods to lighter zebrawoods, we offer an array of high-end, unique materials in all of our products.</p>';
+        // echo '</div>';
+        // echo '</div>';
+        // echo '<div class="col-md-12 text-center pt-4 pb-4">';
+        // echo '<a href="/pages/about" class="btn primary mb-5">Learn More</a>';
+        // echo '</div>';
+        
+        // echo '</div>';
+        // echo '<div class="col-md-6 p-0">';
+        // echo '<img src="//cdn.shopify.com/s/files/1/0280/8004/6214/t/5/assets/Photos-Exotic-Woods.jpg?v=171798210917663730071659978188" alt="" width="100%" height="100%" style="object-fit:cover;">';
+        // echo '</div>';
+        // echo '</div>';
+        echo '</div>';
+        echo '</section>';
+        endif;
+
+        endwhile; endif;
+
+} elseif($layout == 'Skewed Columns'){
 
     if(have_rows('skewed_columns')): while(have_rows('skewed_columns')): the_row();
         echo '<section class="position-relative" style="padding:100px 0;">';
@@ -132,7 +254,7 @@ if($layout == 'Content + Image'){
         echo '</div>';
         echo '</section>';
     endwhile; endif;
-} if($layout == 'Content'){
+} elseif($layout == 'Content'){
     if(have_rows('content')): while(have_rows('content')): the_row();
     echo '<section class="position-relative text-white bg-accent-quaternary" style="padding:100px 0;">';
     echo '<div class="container-fluid">';
@@ -147,7 +269,7 @@ if($layout == 'Content + Image'){
     echo '</section>';
     endwhile; endif;
 
-} if($layout == 'Icons'){
+} elseif($layout == 'Icons'){
     if(have_rows('icons')): while(have_rows('icons')): the_row();
         $bgImg = get_sub_field('background_image');
         $style = get_sub_field('style');
@@ -189,7 +311,7 @@ if($layout == 'Content + Image'){
 
         echo '</section>';
     endwhile; endif;
-} if($layout == 'Listings'){
+} elseif($layout == 'Listings'){
     if(have_rows('listings')): while(have_rows('listings')): the_row();
         echo '<section class="position-relative" style="padding:150px 0;">';
         echo '<div class="container">';
@@ -224,7 +346,7 @@ if($layout == 'Content + Image'){
         echo '</div>';
         echo '</section>';
     endwhile; endif;
-} if($layout == 'Testimonials'){
+} elseif($layout == 'Testimonials'){
     if(have_rows('testimonials')): while(have_rows('testimonials')): the_row();
     echo '<section class="position-relative bg-accent-quinary" style="padding:150px 0;">';
 
@@ -267,7 +389,7 @@ if($layout == 'Content + Image'){
         echo '</div>';
         echo '</section>';
     endwhile; endif;
-} if($layout == 'Contact'){
+} elseif($layout == 'Contact'){
     if(have_rows('contact')): while(have_rows('contact')): the_row();
         echo '<section class="position-relative bg-accent-quinary" style="padding-top:500px;padding-bottom:100px;">';
 
